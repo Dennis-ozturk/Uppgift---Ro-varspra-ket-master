c4808db9c2e54fa331afa6df8db0d452ad7b56a1
itgsod-dennis-ozturk <dennis.ozturk@elev.it-gymnasiet.se>
2015-05-19 15:16:01 +0200

    börjat koda

---
+++ lib/rovarizer.rb
@@ -0,0 +1,8 @@
+def rovarizer(cleartext:)
+  consonants = ["b", "c", "d", "f", "g", "h", "j", "k", "l", "m", "n", "p", "q", "r", "s", "t", "v", "w", "x", "z"]
+  vowels = ["a", "e","i", "o","u","y","å","ä","ö"]
+  i = 0
+  sentence = ["Hej"]
+
+  
+end
